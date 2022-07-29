3b852058d0ee08f2cfd97920d434b6adae3e846c
MoisesHMQ <mendes.moisesmgs@gmail.com>
2022-07-29 13:20:16 -0300

    pacientes cadastro

---
+++ Api_homeCare.js
@@ -0,0 +1,22 @@
+const { request, response } = require('express');
+const express = require('express')
+const app = express();
+var uuid = require('uuid');
+
+const pacientes = [];
+
+app.post('/pacientes/cadastro', (request, response) => {
+    const validarPacientes = pacientes.find((validacao) => validacao.nºficha == request.body.nºficha)
+        if (validarPacientes){
+            return response.send("Status: Paciente já Existe.")}
+        
+        pacientes.push({
+        id: uuid.v4(),
+        nome: request.body.Nome,
+        nºficha: request.body.nºficha,
+        senha:request.bory.senha
+    })
+    return response.send("Status: Paciente Cadastrado")
+})
+
+
